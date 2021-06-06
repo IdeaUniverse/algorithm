69311d752f749c4b3d16ac4d8c44b692d6c27b58
Jack <liujunting@wallstreetcn.com>
2021-06-06 16:39:37 +0800

    optimize

--- src/leetcode/Q1.java
+++ src/leetcode/Q1.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import common.Test;
 import common.Utils;
 
 import java.util.Arrays;
@@ -10,9 +11,9 @@ import java.util.Random;
 /**
  * https://leetcode-cn.com/problems/two-sum
  */
-public class Q1 {
+public class Q1 implements Test {
 
-    public static int[] run(int[] array, int sum){
+    public static int[] twoSum(int[] array, int sum){
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < array.length; i++) {
             map.put(array[i], i);
@@ -26,7 +27,8 @@ public class Q1 {
         return new int[]{};
     }
 
-    public static void validate(){
+    @Override
+    public boolean test() {
         int[] array = Utils.generateIntArray(10);
         int idx1 = new Random().nextInt(array.length);
         int idx2;
@@ -36,17 +38,16 @@ public class Q1 {
         int sum = array[idx1] + array[idx2];
         System.out.println("original array: " + Arrays.toString(array));
         System.out.println("sum = " + sum + ", idx1 = " + idx1 + " idx2 = " + idx2);
-        int[] result = run(array, sum);
+        int[] result = twoSum(array, sum);
         System.out.println("result = " + Arrays.toString(result));
         int a = array[result[0]];
         int b = array[result[1]];
-        System.out.println(a + " + " + b + " = " + (a + b) + ", " + (a + b == sum));
-        System.out.println();
+        System.out.println(a + " + " + b + " = " + (a + b));
+        return a + b == sum;
     }
 
+
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            validate();
-        }
+        Utils.batchTest(new Q1(), 100);
     }
 }
